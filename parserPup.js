73c8f977e67e5e5c95b19ac04f68690f62912e0a
VToreZ <lumisitone@gmail.com>
2019-11-02 23:22:24 +0300

    twitch scraper adaptation

--- parserPup.js
+++ parserPup.js
@@ -1,44 +1,43 @@
 const puppeteer = require('puppeteer');
-
-async function getPic() {
-  const browser = await puppeteer.launch();
+let topDotaArr = [];
+async function getLogins() {
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
-  const loginSelectors = 'a tw-link--inherit';
-  await page.goto('https://www.twitch.tv/directory/game/Dota');
+  const loginSelectors = 'div.preview-card__titles-wrapper.tw-flex-grow-1.tw-flex-shrink-1.tw-full-width > div:nth-child(1) > div > div > p > a';
+  await page.goto(`https://www.twitch.tv/directory/game/Dota%202`);
   await page.waitForSelector(loginSelectors, {timeout: 0});
   const topDotaID = await page.$$eval(loginSelectors, links => links.map(name => name.innerText));
-  console.log(topDotaID);
+  topDotaID.forEach(element => topDotaArr.push(element));
+  console.log(topDotaArr.length);
+  console.log(topDotaArr, topDotaArr.length);
+
   await browser.close();
 };
 
 let scrape = async (login) => {
   let start = Date.now();
+
   const elementSelectors = '.tw-stat__value';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(`https://www.twitch.tv/${login}`);
-  // window.onload = async function() {
-    await page.waitForSelector(elementSelectors, {timeout: 0});
-
-  // const result = await page.evaluate(() => {
-  //   let viewerCount = document.querySelector('.tw-stat__value').innerText;
-  //   return viewerCount;
-  // });
-    const viewersLive = await page.$eval(elementSelectors, selectorIn => selectorIn.innerText);
-  // };
+  await page.waitForSelector(elementSelectors, {timeout: 0});
+  const viewersLive = await page.$eval(elementSelectors, selectorIn => +selectorIn.innerText.match(/\d/g).join(''));
   await browser.close();
+
   let end = Date.now();
-  console.log(end - start);
+  console.log(`Time spended: ${end - start}`);
 
   return viewersLive;
 };
 
-let loginList = [`Dendi`, `rxnexus`, `dota2mc_ru`,`Dendi`, `rxnexus`, `dota2mc_ru`,`Dendi`];
-  for (let i = 0; i < loginList.length; i++) {
-    scrape(loginList[i]).then((value) => {
-    console.log(loginList[i], value);
-  });
-};
-
-
-getPic();
+new Promise(async function(resolve, reject) {
+  await getLogins();
+  await resolve();
+}).finally(() => {
+  for (let j = 0; j < topDotaArr.length - 20; j++) {
+    scrape(topDotaArr[j]).then(value => {
+      console.log(topDotaArr[j], value);
+    });
+  };
+});
